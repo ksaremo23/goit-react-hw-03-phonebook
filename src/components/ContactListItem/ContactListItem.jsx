98eca168566b51eb6c50be79c612cf4e90d14be6
Leicsha Karen Saremo <120180371+ksaremo23@users.noreply.github.com>
2024-04-22 07:25:00 +0800

    1st commit

---
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import css from './ContactListItem.module.css';
+import PropTypes from 'prop-types';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export class ContactListItem extends Component {
+    static propTypes = {
+        filteredContact: PropTypes.object.isRequired,
+        deleteContact: PropTypes.func.isRequired,
+    };
+    handleDelete = () => {
+        const { filteredContact, deleteContact } = this.props;
+        deleteContact(filteredContact.id);
+        Notify.success(
+            `${filteredContact.name} was successfully deleted from your contacts!`, 
+            {position: 'center-top'}
+        );
+    };
+
+    render() {
+        const { filteredContact } = this.props;
+
+        return (
+            <li classNam={css.contactListItem}>
+                <p>{filteredContact.name}:</p>
+                <p classname={css.contactAlign}>{filteredContact.number}</p>
+                <button className={css.btnDelete} onClick={this.handleDelete}>
+                    Delete
+                </button>
+            </li>
+        );
+    }
+}
